a0e890af204004bd492686a38931a7604e112411
Haik <55463651+salampego@users.noreply.github.com>
2022-10-17 20:55:42 +0300

    update

--- src/redux/contacts/operationsContact.js
+++ src/redux/contacts/operationsContact.js
@@ -20,7 +20,6 @@ export const addContact = createAsyncThunk(
   'contacts/addContact',
   async (contact, thunkAPI) => {
     try {
-      console.log(contact);
       const response = await axios.post('/contacts', { ...contact });
       return response.data;
     } catch (e) {
