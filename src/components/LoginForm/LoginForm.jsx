0a818b0368b0e496ac994b47d3d0ea86aa43840c
Haik <55463651+salampego@users.noreply.github.com>
2022-10-17 04:08:55 +0300

    update

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,13 +1,10 @@
-import Notiflix from 'notiflix';
 import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { login } from 'redux/auth/operations';
-import { selectIsLoggedIn } from 'redux/selectors';
 
 import s from './LoginForm.module.css';
 export default function LoginForm() {
   const dispatch = useDispatch();
-  const isLoggedIn = useSelector(selectIsLoggedIn);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -25,9 +22,6 @@ export default function LoginForm() {
   const handleSubmit = e => {
     e.preventDefault();
     dispatch(login({ email, password }));
-    if (isLoggedIn) {
-      Notiflix.Notify.success(`Welcome back`);
-    }
     setEmail('');
     setPassword('');
   };
